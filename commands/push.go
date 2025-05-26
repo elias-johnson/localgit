8dda1aa06a809cc88b7a14f76d295eb2ae1a2332
Elias Johnson <eliascarljohnson@gmail.com>
2025-05-25 22:09:12 -0700

    feat: creates remote directory and current directory

--- commands/push.go
+++ commands/push.go
@@ -3,6 +3,7 @@ package commands
 import (
     "fmt"
     "os"
+    "path/filepath"
 )
 
 /**
@@ -19,7 +20,7 @@ func Push() {
     }
     target := home + "/localgit"
 
-    // Checks if remote directory exists
+    // Initializes remote directory if it does not exist
     _, err = os.Stat(target)
     if err != nil {
         err = os.Mkdir(target, 0755)
@@ -27,5 +28,29 @@ func Push() {
             fmt.Println("An error occurred while initializing remote directory:", err)
             return
         }
+    }
+
+    // Extracts the working directory
+    wd, err := os.Getwd()
+    if err != nil {
+        fmt.Println("An error occurred while identifying working directory:", err)
+        return
+    }
+    source := filepath.Base(wd)
+
+
+    // Checks if repo already exists within remote directory
+    fullTarget := target + "/" + source
+    _, err = os.Stat(fullTarget)
+    if err != nil {
+        err = os.Mkdir(fullTarget, 0755)
+        if err != nil {
+            fmt.Println("An error occurred while pushing repo to remote directory:", err)
+            return
+        }
+
+        // TODO - recursively copy contents from repo to remote directory
+    } else {
+        // TODO - check contents in remote directory before copying over changes
     }
 }
