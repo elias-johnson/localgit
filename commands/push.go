e6a27a7221c19388a30c096ebc1474a5bd9b95f9
Elias Johnson <eliascarljohnson@gmail.com>
2025-05-26 14:58:05 -0700

    in progress

--- commands/push.go
+++ commands/push.go
@@ -1,18 +1,12 @@
 package commands
 
-import (
-    "fmt"
-    "os"
-    "path/filepath"
-)
-
 /**
  * Pushes changes from the cwd to a pseudo-remote repo.
  *
  * Creates the pseudo-remote localgit directory if it does not exist.
  */
 func Push() {
-    // Retrieves home directory
+    // Retrieves target directory
     home, err := os.UserHomeDir()
     if err != nil {
         fmt.Println("An error occurred while accessing home directory:", err)
@@ -31,13 +25,12 @@ func Push() {
     }
 
     // Extracts the working directory
-    wd, err := os.Getwd()
+    dirPath, err := os.Getwd()
     if err != nil {
         fmt.Println("An error occurred while identifying working directory:", err)
         return
     }
-    source := filepath.Base(wd)
-
+    source := filepath.Base(dirPath)
 
     // Checks if repo already exists within remote directory
     fullTarget := target + "/" + source
@@ -48,9 +41,53 @@ func Push() {
             fmt.Println("An error occurred while pushing repo to remote directory:", err)
             return
         }
+    }
 
-        // TODO - recursively copy contents from repo to remote directory
-    } else {
-        // TODO - check contents in remote directory before copying over changes
+    copyRepo(dirPath, fullTarget)
+}
+
+func copyRepo(dirPath, fullTarget string) {
+    entries, err := os.ReadDir(dirPath)
+	if err != nil {
+		fmt.Println("An error occurred while reading a directory:", err)
+		return
+	}
+
+    for _, entry := range entries {
+        if !entry.IsDir() {
+            // Opens source file for reading
+            sourcePath := filepath.Join(dirPath, entry.Name())
+            source, err := os.Open(sourcePath)
+            if err != nil {
+                fmt.Println("An error occurred while reading source file:", err)
+                return
+            }
+
+            // Creates destination file for writing
+            destinationFile := fullTarget + "/" + entry.Name()
+            destination, err := os.Create(destinationFile)
+            if err != nil {
+                fmt.Println("An error occurred while creating destination file:", err)
+                return
+            }
+
+            // Copies contents of source to destination
+            _, err = io.Copy(destination, source)
+            if err != nil {
+                fmt.Println("An error occurred while copying a file:", err)
+                return
+            }
+        } else {
+            // Makes new directory
+            destinationDir := fullTarget + "/" + entry.Name()
+            err = os.Mkdir(destinationDir, 0755)
+            if err != nil {
+                fmt.Println("An error occurred while copying over a directory:", err)
+                return
+            }
+
+            updatedDirPath := dirPath + "/" + entry.Name()
+            copyRepo(updatedDirPath, fullTarget)
+        }
     }
 }
