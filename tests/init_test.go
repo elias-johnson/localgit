72aa60e92ee8f0efac76281fddff964b760e2382
Elias Johnson <eliascarljohnson@gmail.com>
2025-05-26 17:49:36 -0700

    fix indentation

--- tests/init_test.go
+++ tests/init_test.go
@@ -1,50 +1,50 @@
 package tests
 
 import (
-	"testing"
-	"localgit/commands"
-	"os"
+    "testing"
+    "localgit/commands"
+    "os"
 )
 
 func TestInit(t *testing.T) {
     commands.Init()
 
-	_, err := os.Stat(".lit")
+    _, err := os.Stat(".lit")
     if err != nil {
         t.Errorf("Init did not create a localgit directory.")
     }
 
-	_, err = os.Stat(".lit/branches")
+    _, err = os.Stat(".lit/branches")
     if err != nil {
         t.Errorf("Init did not create a branches directory within the localgit directory.")
     }
 
-	_, err = os.Stat(".lit/hooks")
+    _, err = os.Stat(".lit/hooks")
     if err != nil {
         t.Errorf("Init did not create a hooks directory within the localgit directory.")
     }
 
-	_, err = os.Stat(".lit/info")
+    _, err = os.Stat(".lit/info")
     if err != nil {
         t.Errorf("Init did not create an info directory within the localgit directory.")
     }
 
-	_, err = os.Stat(".lit/objects")
+    _, err = os.Stat(".lit/objects")
     if err != nil {
         t.Errorf("Init did not create an objects directory within the localgit directory.")
     }
 
-	_, err = os.Stat(".lit/refs")
+    _, err = os.Stat(".lit/refs")
     if err != nil {
         t.Errorf("Init did not create a refs directory within the localgit directory.")
     }
 
-	_, err = os.Stat(".lit/HEAD")
+    _, err = os.Stat(".lit/HEAD")
     if err != nil {
         t.Errorf("Init did not create a HEAD file within the localgit directory.")
     }
 
-	_, err = os.Stat(".lit/info/exclude")
+    _, err = os.Stat(".lit/info/exclude")
     if err != nil {
         t.Errorf("Init did not create an exclude file within the info directory.")
     }
