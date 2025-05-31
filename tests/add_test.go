6b8599e319a2f2e3490ece3771f83ac96fecec59
Elias Johnson <eliascarljohnson@gmail.com>
2025-05-30 19:27:57 -0700

    update test

--- tests/add_test.go
+++ tests/add_test.go
@@ -2,13 +2,7 @@ package tests
 
 import (
     "testing"
-    "localgit/commands"
 )
 
 func TestAdd(t *testing.T) {
-    commands.Init()
-
-	// TODO add a txt file or smth
-
-	commands.Add([]string{})
 }
